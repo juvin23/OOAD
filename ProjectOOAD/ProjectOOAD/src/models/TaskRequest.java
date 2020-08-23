87486d854fc8bbcf0797db46581e90d8290136eb
VickyIsando <70099584+VickyIsando@users.noreply.github.com>
2020-08-23 20:23:57 +0700

    Add files via upload

---
+++ ProjectOOAD/ProjectOOAD/src/models/TaskRequest.java
@@ -0,0 +1,201 @@
+package models;
+
+import java.sql.ResultSet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+//import java.util.List;
+import java.util.UUID;
+
+import com.mysql.jdbc.PreparedStatement;
+
+import connection.Connector;
+//import controllers.TaskRequestHandler;
+public class TaskRequest {
+	private UUID id;
+	private UUID workerID;
+	private UUID supervisorID;
+	private String title;
+	private String note;
+
+	
+	//constructor
+	public TaskRequest(UUID id, UUID workerID, UUID supervisorID, String title, String note) {
+		super();
+		this.id = id;
+		this.workerID = workerID;
+		this.supervisorID = supervisorID;
+		this.title = title;
+		this.note = note;
+	}
+	
+	//done
+	//get(id)
+	public static TaskRequest get(String id) {
+		String query = "SELECT * from task_requests where id = ?";
+		
+		try {
+			PreparedStatement ps = (PreparedStatement) Connector.getConnection().prepareStatement(query);
+			ps.setString(1, id);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			
+			String trID = rs.getString("id");
+			String workerID = rs.getString("workerID");
+			String supervisorID = rs.getString("supervisorID");
+			String title = rs.getString("title");
+			String note = rs.getString("note");
+			
+			return new TaskRequest(UUID.fromString(trID), UUID.fromString(workerID), UUID.fromString(supervisorID), title, note);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	//should be done
+	//getAll(userID)
+	public static ArrayList<TaskRequest> getAll(String userID){
+		String query = "SELECT * FROM task_requests";
+		ArrayList<TaskRequest> taskRequestList = new ArrayList<TaskRequest>();
+		
+		try {
+			PreparedStatement ps;
+			ps = (PreparedStatement) Connector.getConnection().prepareStatement(query);
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				String trID = rs.getString("id");
+				String workerID = rs.getString("workerID");
+				String supervisorID = rs.getString("supervisorID");
+				String title = rs.getString("title");
+				String note = rs.getString("note");
+				
+				TaskRequest temp = new TaskRequest(UUID.fromString(trID), UUID.fromString(workerID), UUID.fromString(supervisorID), title, note);
+				taskRequestList.add(temp);
+				
+			}
+			return taskRequestList;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//ps.setString(1, id);
+		return null;
+		
+	}
+	//gatau ini salah fix, masi belom paham 
+	
+	//delete()
+	public UUID delete() {
+		String query = "DELETE FROM task_requests"
+				+ "WHERE = ?";
+
+		try {
+			PreparedStatement ps = (PreparedStatement) Connector.getConnection().prepareStatement(query);
+			
+			ps.setString(1, this.id.toString());
+			ps.execute();
+			
+			System.out.println("Successfully Deleted!");
+			//return 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		return this.id;
+	}
+	
+	//save()
+	public TaskRequest save() {
+		
+		String query = "INSERT INTO task_requests VALUES(?, ?, ? , ?, ?)";
+		
+		try {
+			PreparedStatement ps = (PreparedStatement) Connector.getConnection().prepareStatement(query);
+			ps.setString(1, this.id.toString());
+			ps.setString(2, this.workerID.toString());
+			ps.setString(3, this.supervisorID.toString());
+			ps.setString(4, this.title);
+			ps.setString(5, this.note);
+			
+			//ResultSet rs = 
+			ps.execute();
+			//rs.next();
+			System.out.println("Successfuly inserted!!");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return this;
+	}
+	//gw ga nemu yang fungsi update dimana kepakenya 
+	public TaskRequest update() {
+		String query = "UPDATE task_requests SET"
+				+ " workerID = ?, supervisorID = ?, title = ?, note = ?";
+		
+		try {
+			PreparedStatement ps = (PreparedStatement) Connector.getConnection().prepareStatement(query);
+			ps.setString(1, this.workerID.toString());
+			ps.setString(2, this.supervisorID.toString());
+			ps.setString(3, title);
+			ps.setString(4, note);
+			
+			
+			ps.execute();
+			
+			return this;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+		
+	}
+	
+	
+	
+	//setter getter
+	public UUID getWorkerID() {
+		return workerID;
+	}
+	
+	public void setWorkerID(UUID workerID) {
+		this.workerID = workerID;
+	}
+	public UUID getSupervisorID() {
+		return supervisorID;
+	}
+	public void setSupervisorID(UUID supervisorID) {
+		this.supervisorID = supervisorID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	
+	
+	
+}
