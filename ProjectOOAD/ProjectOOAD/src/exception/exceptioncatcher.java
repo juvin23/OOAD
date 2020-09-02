ff11826a62ec489fdc276d386b694bdc767f0a70
VickyIsando <70099584+VickyIsando@users.noreply.github.com>
2020-09-02 22:16:30 +0700

    Add files via upload

---
+++ ProjectOOAD/ProjectOOAD/src/exception/exceptioncatcher.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class exceptioncatcher extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public exceptioncatcher(String notes) {
+		// TODO Auto-generated constructor stub
+		super(notes);
+	}
+	
+}
