df81bd6f9b59b9f4859736306695867086dce58a
VickyIsando <70099584+VickyIsando@users.noreply.github.com>
2020-09-07 18:25:11 +0700

    Update TaskRequestHandler.java

--- ProjectOOAD/ProjectOOAD/src/controllers/TaskRequestHandler.java
+++ ProjectOOAD/ProjectOOAD/src/controllers/TaskRequestHandler.java
@@ -8,68 +8,53 @@ import java.util.UUID;
 import models.Task;
 import models.TaskRequest;
 import models.User;
-import views.AllTaskRequestDisplay;
+//import views.AllTaskRequestDisplay;
 
 public class TaskRequestHandler {
 	public TaskRequestHandler() {
 		// TODO Auto-generated constructor stub
 	}
 	
-	
-	
-	
-	//gatau ini gayakin
-	public ArrayList<TaskRequest> getAllTaskRequest(){
+	public static ArrayList<TaskRequest> getAllTaskRequest(){
 		User user = User.get("id");
 		return TaskRequest.getAll(user.getUserID().toString());
 	}
 	
-	public TaskRequest getTaskRequest(String taskRequestId) {
+	public static TaskRequest getTaskRequest(String taskRequestId) {
 		return TaskRequest.get(taskRequestId);
 	}
 	
-	
-	
-	// create task request
 	public static TaskRequest createTaskRequest(String title, UUID supervisorID, UUID workerID, String note) {
 		UUID id = UUID.randomUUID();
 		TaskRequest tr =  new TaskRequest(id, workerID, supervisorID, title, note);
-		
 	
 		return tr.save();
 	}
 	
-	
-	
-	//accept task request
 	public static TaskRequest acceptTaskRequest(String id) {
 		TaskRequest tr = TaskRequest.get(id);
-		
-		
 		
 		if (tr == null) {
 			System.out.println("Task Request not found");
 			return null;
 		}
-		else {
-			String title = tr.getTitle();
-			String supervisorId = tr.getSupervisorID().toString();
-			String workerId = tr.getWorkerID().toString();
-			String note = tr.getNote();
-			String trId = tr.delete().toString();
-			Task task = Task.create(title, UUID.fromString(supervisorId), UUID.fromString(workerId), note); //do uncomment if the create already created on task class
-			task.save();
-			//String message = "dunno";
-			//createNotification(id, message);
-			
-			String message = "Your task request accepted!";
-			NotificationController.createNotification(UUID.fromString(id), message);
-			System.out.println("Successfully TR: "+ trId +"deleted");
-			System.out.println("Task Request accepted");
-			
-			return tr;
-		}
 		
+		String title = tr.getTitle();
+		String supervisorId = tr.getSupervisorID().toString();
+		String workerId = tr.getWorkerID().toString();
+		String note = tr.getNote();
+		String trId = tr.delete().toString();
+		Task task = Task.create(title, UUID.fromString(supervisorId), UUID.fromString(workerId), note); //do uncomment if the create already created on task class
+		task.save();
+		//String message = "dunno";
+		//createNotification(id, message);
+			
+		String message = "Your task request accepted!";
+		NotificationController.createNotification(UUID.fromString(id), message);
+		System.out.println("Successfully TR: "+ trId +"deleted");
+		System.out.println("Task Request accepted");
+			
+		return tr;
 	}
 	
 	//reject task request
@@ -79,21 +64,11 @@ public class TaskRequestHandler {
 			System.out.println("Not found");
 			return null;
 		}
-		else {
-//			String title = tr.getTitle();
-//			String supervisorId = tr.getSupervisorID().toString();
-//			String workerId = tr.getWorkerID().toString();
-//			String note = tr.getNote();
-//		
-			String trId = tr.delete().toString();
-//			
-			String message = "Your task request rejected!";
-			NotificationController.createNotification(UUID.fromString(id), message);
-			System.out.println("Successfully TR: "+ trId +"deleted");
-			return tr;
-		}
-		
-		
+		String trId = tr.delete().toString();
+		String message = "Your task request rejected!";
+		NotificationController.createNotification(UUID.fromString(id), message);
+		System.out.println("Successfully TR: "+ trId +"deleted");
+		return tr;
 	}
 	
 }
