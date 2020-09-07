fb9de07e2376104477ede8e0b74aaa023cf310f6
juvin23 <53383961+juvin23@users.noreply.github.com>
2020-09-07 10:33:35 +0700

    Update Env.java

--- ProjectOOAD/ProjectOOAD/src/helpers/Env.java
+++ ProjectOOAD/ProjectOOAD/src/helpers/Env.java
@@ -1,6 +1,14 @@
 package helpers;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import models.User;
+
 public class Env {
+	public static User user;
+	public static Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	
 	public static String DRIVER = "mysql";
 	public static String HOST = "localhost";
 	public static String PORT = "3306";
