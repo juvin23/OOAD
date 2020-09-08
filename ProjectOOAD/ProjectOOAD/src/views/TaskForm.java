a991d97d7509278ce3096a2c55a88291b6ab319d
filbertg <61968172+filbertg@users.noreply.github.com>
2020-09-08 12:39:23 +0700

    Update TaskForm.java

--- ProjectOOAD/ProjectOOAD/src/views/TaskForm.java
+++ ProjectOOAD/ProjectOOAD/src/views/TaskForm.java
@@ -4,23 +4,26 @@ import javax.swing.JPanel;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
+import java.util.UUID;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
+import com.sun.glass.events.MouseEvent;
 
 import controllers.TaskHandler;
 import controllers.TaskRequestHandler;
 import controllers.UserController;
 import helpers.Env;
+import models.Task;
 import models.User;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import javax.swing.JList;
-import javax.swing.ListSelectionModel;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
@@ -29,11 +32,14 @@ import javax.swing.JFrame;
 
 public class TaskForm extends JFrame {
 	private static final long serialVersionUID = 1L;
-	private JList<User> list;
-	private DefaultListModel<User> userlist;
+	private JTable table;
+	private DefaultTableModel userlist;
 	private JTextField title;
-	
+	private ArrayList<Task> temp;
+	private int selRow, selCol;
+	private String value;
 	private static TaskForm tf;
+	
 	
 	public static TaskForm getInstance() {
 		if(tf == null) {
@@ -43,7 +49,8 @@ public class TaskForm extends JFrame {
 	}
 
 	public TaskForm() {
-		User user = Env.user;
+//		User user = Env.user;
+		User user = UserController.getInstance().getByUname("testing");
 		this.setSize(new Dimension(800, 600));
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -62,17 +69,32 @@ public class TaskForm extends JFrame {
 		panel_1.add(panel_2, BorderLayout.CENTER);
 		panel_2.setLayout(null);
 		
-		JScrollPane scrollpane = new JScrollPane();
-		panel_2.add(scrollpane);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setSize(744, 200);
+		scrollPane.setLocation(30, 30);
+		panel_2.add(scrollPane, BorderLayout.CENTER);
 		
-		userlist = new DefaultListModel();
+		userlist = new DefaultTableModel();
+		userlist.setColumnIdentifiers(Env.TASKFORM);
 		if(user.getRole().equals("Supervisor")) {
-			setListModel(UserController.getInstance().getUserByRole("Worker"));
+			userlist.setRowCount(0);
+			ArrayList<User> temp;
+			temp = UserController.getInstance().getUserByRole("Worker");
+			for (User users : temp) {
+				String worker = users.getUsername();
+				
+				userlist.addRow(new Object[] {worker});
+			}
 		}else {
-			setListModel(UserController.getInstance().getUserByRole("Supervisor"));
+			userlist.setRowCount(0);
+			ArrayList<User> temp;
+			temp = UserController.getInstance().getUserByRole("Supervisor");
+			for (User users : temp) {
+				String supervisor = users.getUsername();
+				
+				userlist.addRow(new Object[] {supervisor});
+			}
 		}
-		
-		scrollpane.setViewportView(list);
 		
 		JLabel lblNewLabel_1 = new JLabel("Title");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
@@ -93,11 +115,18 @@ public class TaskForm extends JFrame {
 		note.setBounds(99, 333, 675, 60);
 		panel_2.add(note);
 		
-		list = new JList(userlist);
-		list.setBounds(41, 11, 733, 155);
-		panel_2.add(list);
-		list.setVisibleRowCount(10);
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table = new JTable(userlist);
+		scrollPane.setViewportView(table);
+
+		table.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				selRow = table.getSelectedRow();
+				selCol = table.getSelectedColumn();
+				value = table.getValueAt(selRow, selCol).toString();
+			}
+		});
+
 		
 		JPanel panel3 = new JPanel();
 		panel3.setBorder(new EmptyBorder(20, 10, 20, 10));
@@ -112,10 +141,10 @@ public class TaskForm extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				if(user.getRole().equals("Supervisor")) {
-					TaskHandler.CreateTask(title.getText(), user.getUserID().toString(), list.getSelectedValue().getUserID().toString(), note.getText());
+					TaskHandler.CreateTask(title.getText(), user.getUserID().toString(), UserController.getInstance().getByUname(value).toString(), note.getText());
 				}
 				else {
-					TaskRequestHandler.createTaskRequest(title.getText(), list.getSelectedValue().getUserID(), user.getUserID(), note.getText());
+					TaskRequestHandler.createTaskRequest(title.getText(), UUID.fromString(UserController.getInstance().getByUname(value).toString()), user.getUserID(), note.getText());
 				}
 			}
 		});
@@ -133,10 +162,3 @@ public class TaskForm extends JFrame {
 		
 		this.setVisible(true);
 	}
-
-	private void setListModel(ArrayList<User> list) {
-		for (User u : list) {
-			userlist.addElement(u);
-		}
-	}
-}
