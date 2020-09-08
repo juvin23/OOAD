fe2d53e61d66382cb24cb483cacb47bfe73d8242
VickyIsando <70099584+VickyIsando@users.noreply.github.com>
2020-09-08 09:38:08 +0700

    Update AllTaskRequestDisplay.java

--- ProjectOOAD/ProjectOOAD/src/views/AllTaskRequestDisplay.java
+++ ProjectOOAD/ProjectOOAD/src/views/AllTaskRequestDisplay.java
@@ -19,57 +19,52 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import controllers.TaskRequestHandler;
+import helpers.Env;
 //import helpers.Env;
 //import helpers.Utils;
 
-public class AllTaskRequestDisplay extends JPanel {
+public class AllTaskRequestDisplay extends JFrame {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JFrame frame;
 	private JTable table;
 	private DefaultTableModel model;
 	
-	
-	/**
-	 * Launch the application.
-	 */
-	
-	/**
-	 * Create the application.
-	 */
-	public AllTaskRequestDisplay() {
-		initialize();
+	private static AllTaskRequestDisplay display;
+	public static AllTaskRequestDisplay getInstance(User user) {
+		if(display == null) {
+			display = new AllTaskRequestDisplay(user);
+		}
+		return display;
 	}
+	
+	
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public AllTaskRequestDisplay(User user) {
+		//this.getInstance(user);
+		this.setBounds(100, 100, 450, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		String supervisorID = User.instance.getUserID().toString();
+		String supervisorID = Env.user.getUserID().toString();
 		
 		
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		this.getContentPane().add(panel, BorderLayout.NORTH);
 		
 		JLabel lblNewLabel = new JLabel("All Task Request Display");
 		panel.add(lblNewLabel);
 		
 		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1, BorderLayout.WEST);
+		this.getContentPane().add(panel_1, BorderLayout.WEST);
 		panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JLabel lblNewLabel_1 = new JLabel("SupervisorID: "+ supervisorID);
 		panel_1.add(lblNewLabel_1);
 		
 		JPanel panel_2 = new JPanel();
-		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
+		this.getContentPane().add(panel_2, BorderLayout.SOUTH);
 		
 		JButton btnNewButton = new JButton("Accept");
 		panel_2.add(btnNewButton);
@@ -81,37 +76,36 @@ public class AllTaskRequestDisplay extends JPanel {
 		panel_2.add(btnNewButton_2);
 		
 		JPanel panel_3 = new JPanel();
-		frame.getContentPane().add(panel_3, BorderLayout.CENTER);
+		this.getContentPane().add(panel_3, BorderLayout.CENTER);
 		
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String id = (String) table.getValueAt(table.getSelectedRow(), 0);
 				TaskRequestHandler.acceptTaskRequest(id);
-				loadRow();
+				loadRow(user);
 			}
 		});
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String id = (String) table.getValueAt(table.getSelectedRow(), 0);
 				TaskRequestHandler.rejectTaskRequest(id);
-				loadRow();
+				loadRow(user);
 			}
 		});
 		btnNewButton_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				//calling main menu
-				
+				MainDisplay.getInstance();
 			}
 		});
 		
 		//table panel
 		JScrollPane scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		this.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
 
 		model = new DefaultTableModel();
-		//model.setColumnIdentifiers(Utils.TASKREQUESTHEADER);
-		loadRow();
+		loadRow(user);
 		table = new JTable(new DefaultTableModel(
 			new Object[][] {
 			},
@@ -120,18 +114,15 @@ public class AllTaskRequestDisplay extends JPanel {
 			}
 		));
 		scrollPane.setViewportView(table);
+	}
 
 	
-	}
-	
-	
-	
-	public void loadRow() {
+	public void loadRow(User user) {
 		model.setRowCount(0);
 		ArrayList<TaskRequest> trList = TaskRequestHandler.getAllTaskRequest();
 		for(TaskRequest tr : trList) {
 			
-			if(tr.getSupervisorID().toString().equals(User.instance.getUserID().toString())) {
+			if(tr.getSupervisorID().toString().equals(user.getUserID().toString())) {
 				String taskID = tr.getId().toString();
 				String workerID = User.get(tr.getWorkerID().toString()).getUsername();
 				String title = tr.getTitle();
